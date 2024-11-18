1ad3a6379420f633c75d3399766415adfaf84295
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2024-11-18 23:53:31 +0530

    1.2 setting up

---
+++ src/test/java/com/learningspringboot/saurav/week1Introduction/IntroductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learningspringboot.saurav.week1Introduction.IntroductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
