6abb94113daef30e744cb1ae4e8974231ade2439
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2024-11-21 23:10:20 +0530

    Presentation Layer(Controller)

---
+++ src/main/java/com/begineer/springbootweb/springbootweb/SpringBootAndSpringBootWebApplication.java
@@ -0,0 +1,13 @@
+package com.begineer.springbootweb.springbootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAndSpringBootWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAndSpringBootWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/begineer/springbootweb/springbootweb/SpringBootAndSpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.begineer.springbootweb.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAndSpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
