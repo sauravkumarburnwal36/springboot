d6aa5526a91b309a8d69906f8be3163d96408602
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2024-11-23 01:43:45 +0530

    Persistence layer using h2 inmemory database

--- src/main/java/com/begineer/springbootweb/springbootweb/controllers/EmployeeController.java
+++ src/main/java/com/begineer/springbootweb/springbootweb/controllers/EmployeeController.java
@@ -1,28 +1,34 @@
 package com.begineer.springbootweb.springbootweb.controllers;
 
 import com.begineer.springbootweb.springbootweb.dto.EmployeeDTO;
+import com.begineer.springbootweb.springbootweb.entities.EmployeeEntity;
+import com.begineer.springbootweb.springbootweb.repositories.EmployeeRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/employees")
 public class EmployeeController {
 
+    private EmployeeRepository employeeRepository;
+    public EmployeeController(EmployeeRepository employeeRepository){
+        this.employeeRepository=employeeRepository;
+    }
     @GetMapping("/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable(name="employeeId") Long id){
-        return new EmployeeDTO(id,"Saurav","saurav09@gmail.com",26, LocalDate.of(2021,8,5),true);
+    public EmployeeEntity getEmployeeById(@PathVariable(name="employeeId") Long id){
+        return employeeRepository.findById(id).orElse(null);
     }
 
         @GetMapping()
-    public String getEmployee(@RequestParam(required = false)Integer age,@RequestParam(required = false) String sortBy)
+    public List<EmployeeEntity> getEmployee(@RequestParam(required = false)Integer age, @RequestParam(required = false) String sortBy)
         {
-            return "Hi "+age+" age "+sortBy+" sortBy ";
+            return employeeRepository.findAll();
         }
         @PostMapping()
-    public EmployeeDTO updateEmployeeById(@RequestBody EmployeeDTO employeeDTO){
-        employeeDTO.setId(100L);
-        return employeeDTO;
+    public EmployeeEntity updateEmployeeById(@RequestBody EmployeeEntity employeeEntity){
+        return employeeRepository.save(employeeEntity);
     }
     @PutMapping()
     public String createNewEmployee(){
