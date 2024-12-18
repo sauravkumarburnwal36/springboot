4227637feba4f4c2ff665c5093765de5aec3591b
sauravkumarburnwal36 <sauravkumarburnwal36@gmail.com>
2024-12-18 15:42:53 +0530

    Setup SpringBoot Project

--- src/main/java/com/begineer/springbootweb/springbootweb/exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.begineer.springbootweb.springbootweb.exceptions;
-
-public class ResourceNotFoundException extends RuntimeException{
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
